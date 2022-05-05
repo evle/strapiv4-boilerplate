7553541f5f8eb0c280adf78a75def10bb6c6640a
xiangyu xy li <xiangyu.xy.li@cn.pwc.com>
2022-05-05 21:41:24 +0800

    feat: add transformer plugin

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,6 @@ module.exports = ({ env }) => ({
         prefix: '/api/',
         responseTransforms: {
           removeAttributesKey: true,
-          removeDataKey: true,
         }
       }
     },
