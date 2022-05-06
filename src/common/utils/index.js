0d203c52a6b61681e13cf19db3d03a85dff8561f
xiangyu xy li <xiangyu.xy.li@cn.pwc.com>
2022-05-06 23:13:24 +0800

    fixbug

--- src/common/utils/index.js
+++ src/common/utils/index.js
@@ -8,10 +8,10 @@ const {
 const standardizeRespond = (ctx) => {
   if (!ctx.originalUrl.includes("/api/") && ctx.body.message) return;
 
-  if (!ctx.body.data) {
-    let tmp = ctx.body;
-    ctx.body = { data: tmp };
-  }
+  // if (!ctx.body.data) {
+  //   let tmp = ctx.body;
+  //   ctx.body = { data: tmp };
+  // }
 
   ctx.body.status =
     ctx.response.status !== 200 ? DEFAULT_FAIL_STATUS : DEFAULT_SUCCESS_STATUS;
